7394d79ad7318aae9989f8456c6689ddb286e9e5
Mihir <mwadekar2000@gmail.com>
2021-04-19 12:42:17 -0700

    Changed deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,18 @@
+const { ethers } = require("ethers");
+
 async function main() {
     const Token = await ethers.getContractFactory("CosmicToken");
     const MasterChef = await ethers.getContractFactory("MasterChef");
+    const Presale = await ethers.getContractFactory("Presale")
     
     // Start deployment, returning a promise that resolves to a contract object
     const token = await Token.deploy();
     const masterChef = await MasterChef.deploy(token.address, 0);
+    const presale = await Presale.deploy(token.address, 0);
 
     console.log("Token deployed to address:", token.address);
     console.log("MasterChef deployed to address:", masterChef.address);
+    console.log("Presale deployed to address:", presale.address);
 }
 
 main()
